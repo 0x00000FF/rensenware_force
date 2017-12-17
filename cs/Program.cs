1c98b209459a226bd48a7f514959e156c4b823f5
P.Knowledge <doctor@kangnang.net>
2017-12-18 02:16:01 +0900

    moved to C# directory

--- cs/Program.cs
+++ cs/Program.cs
@@ -7,9 +7,6 @@ namespace rensenWare_forcer
 {
     static class Program
     {
-        /// <summary>
-        /// 해당 응용 프로그램의 주 진입점입니다.
-        /// </summary>
         [STAThread]
         static void Main()
         {
